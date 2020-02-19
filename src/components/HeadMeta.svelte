66ef567eb3a4f71297d3eb0251d71d74e1f6247f
sandrina-p <sandrin4pereir4@gmail.com>
2020-02-19 13:28:13 +0000

    Fix cover url

--- src/components/HeadMeta.svelte
+++ src/components/HeadMeta.svelte
@@ -1,8 +1,8 @@
 <script>
-  const site = 'htpps://sandrina-p.net'
+  const site = 'https://sandrina-p-beta.netlify.com'
   const title = 'Sandrina Pereira - UX Enginner';
   const description = `I'm a UX Enginner who helps to turn ideas into accessible experiences.`;
-  const imageCover = 'cover_light.png';
+  const coverUrl = `${site}/cover_light.png`;
 </script>
 
 <svelte:head>
@@ -13,23 +13,23 @@
   <meta name="keywords" content="UX Engineer, frontend developer, React, CSS, accessibility, usability">
   <meta name="description" content={description}>
   
-  <meta name="image" content={ imageCover }>
+  <meta name="image" content={ coverUrl }>
   <link rel="icon" type="image/png" href="favicon16.png" sizes="16x16">
   <link rel="icon" type="image/png" href="favicon32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="favicon64.png" sizes="64x64">
   <link rel="icon" type="image/png" href="favicon180.png" sizes="180x180">
   <link rel="apple-touch-icon" href="favicon512.png" sizes="512x512">
 
-  <meta name="twitter:card" content="summary">
+  <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:creator" content="@a_sandrina_p">
   <meta name="twitter:title" content={ title }>
   <meta name="twitter:description" content={ description }>
-  <meta name="twitter:image" content={ imageCover }>
+  <meta name="twitter:image" content={ coverUrl }>
 
   <meta property="og:url" content={ site }>
   <meta property="og:title" content={ title }>
   <meta property="og:description" content={ description }>
-  <meta property="og:image" content={ imageCover }>
+  <meta property="og:image" content={ coverUrl }>
 
   <script type="application/ld+json">
 [{"@context":"http://schema.org","@type":"WebSite","url":"https://sandrina-p.net","name":"Sandrina Pereira, UX Engineer"}]
