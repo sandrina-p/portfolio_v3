d5f9a5f5a46e1b55f02b329cae9c450ce20ec738
sandrina-p <sandrin4pereir4@gmail.com>
2020-02-24 09:48:14 +0000

    tablet - add breakpoint

--- src/theme/breakpoints.js
+++ src/theme/breakpoints.js
@@ -1,9 +1,11 @@
 // Media queries to be used by PostCSS and store/responsive (JS).
 
 const breakpoints = {
-  md: '(min-width: 48em)', // 768px (~tablet)
   'max-xs': '(max-width: 20em)', // 320px (iphone5)
+  md: '(min-width: 48em)', // 768px (~tablet)
+  lg: '(min-width: 64.0625em)', // 1024px + 1px - (laptop)
   'max-md': '(max-width: 47.9375em)', // 768px - 1px
+  'only-tablet': '(min-width: 48em) and (max-width: 64em)', // 768px - 1024px (tablet)
 };
 
 module.exports = breakpoints;
