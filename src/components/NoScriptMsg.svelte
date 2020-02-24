7b163cbf7ab5a1fec004309200c83587458b3922
sandrina-p <sandrin4pereir4@gmail.com>
2020-02-24 12:31:48 +0000

    Better noScript message

--- src/components/NoScriptMsg.svelte
+++ src/components/NoScriptMsg.svelte
@@ -2,9 +2,7 @@
 import { EMAIL_URL } from '../data/misc.js';
 </script>
 <noscript>
-  <p class="g-msg" style="margin-top: 16px; display: block;">
-    Well, this website works better with Javascript enabled.
-    <br/>
-    Fear not, you can still reach me by <a href={EMAIL_URL} class="u-link">e-mail</a>!
+  <p class="g-msg" style="margin-top: 16px; text-align: left; display: block;">
+    This would work better with Javascript enabled. Fear not, you can still reach me by <a href={EMAIL_URL} class="u-link">e-mail</a>!
   </p>
 </noscript>
