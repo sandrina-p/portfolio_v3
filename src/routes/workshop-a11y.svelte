69d8e0dbd72b0ddb236ddc84e3401575966002e0
Sandrina Pereira <sandrin4pereir4@gmail.com>
2021-04-13 11:12:02 +0100

    WK A11Y: Update seats

--- src/routes/workshop-a11y.svelte
+++ src/routes/workshop-a11y.svelte
@@ -1168,7 +1168,7 @@
 
         <li>
           <Accordion summary="How many attendes will be there?">
-            There are 15 seats available. I aim to keep the group small, 
+            There are 12 seats available. I aim to keep the group small, 
             so that people don't feel shy/intimidated in participating.
             This also allows me to have the time to give you the attention
             and care you deserve to clarify your questions.
