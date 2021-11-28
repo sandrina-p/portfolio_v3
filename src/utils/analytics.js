0d4e55f406a228f7dedc6e82334d4077ddd7d369
Sandrina Pereira <sandrin4pereir4@gmail.com>
2021-11-28 15:11:48 +0000

    Activate analytics

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -1,13 +1,13 @@
 import splitbee from '@splitbee/web';
 
-// splitbee.init({
-//   disableCookie: true,
-// });
+splitbee.init({
+  disableCookie: true,
+});
 
 export function sendTrack(eventName, extra = '') {
-  // splitbee.track(eventName, { extra: extra.toString() });
+  splitbee.track(eventName, { extra: extra.toString() });
 }
 
 export function trackUser(options) {
-  // splitbee.user.set(options);
+  splitbee.user.set(options);
 }
