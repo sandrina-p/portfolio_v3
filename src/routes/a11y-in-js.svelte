682bb51b4c36a13918a0d0f1926d2fb834bf7178
Sandrina Pereira <sandrin4pereir4@gmail.com>
2022-06-25 12:25:59 +0100

    feat: Add a11-in-js route to codesandbox

---
+++ src/routes/a11y-in-js.svelte
@@ -0,0 +1,5 @@
+<script context="module">
+  export async function preload() {
+    return this.redirect(301, 'https://codesandbox.io/s/a11y-in-js-workshop-reactsummit-zv46i9?file=/src/App.js')
+  }
+</script>
